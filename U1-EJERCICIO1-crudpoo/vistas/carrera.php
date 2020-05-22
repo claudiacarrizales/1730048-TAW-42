7bac189fb7611786ecea3ccf49c9d02c2a53c778
claudiacarrizlaes <1730048@upv.edu.mx>
2020-05-22 09:18:15 -0500

    views uni-carrera

---
+++ U1-EJERCICIO1-crudpoo/vistas/carrera.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="jumbotron">
+        <h2>Carrera</h2>
+        <!--FORMULARIO DEL REGISTRO DE ESTUDIANTES-->
+    </div>
+    <div class="col-md-6 col-md-offset-3">
+        <div class="form-horizontal" style="">
+
+                </div>  
+                    <div class="form-group">
+                        <label class=" col-sm-2 control-label" for="txt_nomC">NOMBRE:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="txt_nomC" value="">
+                        </div>            
+                    </div>
+                   
+                    
+
+                    <div class="form-group">
+                        <div class="col-md-12 col-md-off-set-3">
+                        
+                            <input type="submit" class="btn btn-primary form-control" name="" value="Entrar">
+                        
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+            
+        </div>
+    </div>
+    
+</div>
---
+++ U1-EJERCICIO1-crudpoo/vistas/universidad.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="jumbotron">
+        <h2>Universidad</h2>
+        <!--FORMULARIO DEL REGISTRO DE ESTUDIANTES-->
+    </div>
+    <div class="col-md-6 col-md-offset-3">
+        <div class="form-horizontal" style="">
+
+                </div>  
+                    <div class="form-group">
+                        <label class=" col-sm-2 control-label" for="txt_nomU">NOMBRE:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="txt_nomU" value="">
+                        </div>            
+                    </div>
+                   
+                    
+                   <!-- <div class="form-group">
+                        <label class=" col-sm-2 control-label" for="txt_contral">PASSWORD:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="txt_contral" value="">
+                        </div>         
+                    </div>-->
+
+                    <div class="form-group">
+                        <div class="col-md-12 col-md-off-set-3">
+                        
+                            <input type="submit" class="btn btn-primary form-control" name="" value="Entrar">
+                        
+                        </div>
+                    </div>
+
+                </div>
+            </form>
+            
+        </div>
+    </div>
+    
+</div>
