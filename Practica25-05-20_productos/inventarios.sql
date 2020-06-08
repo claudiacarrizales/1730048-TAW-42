1470ecc18cba4467b291ffa31f91c994365f9276
claudiacarrizlaes <1730048@upv.edu.mx>
2020-06-08 07:32:33 -0500

    inentario en serv

--- Practica25-05-20_productos/inventarios.sql
+++ Practica25-05-20_productos/inventarios.sql
@@ -1,33 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 05-06-2020 a las 14:49:11
--- Versión del servidor: 5.7.30-0ubuntu0.18.04.1
--- Versión de PHP: 7.2.24-0ubuntu0.18.04.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `inventarios`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `firstname` varchar(50) NOT NULL,
@@ -43,7 +13,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `user_name`, `user_password`, `user_email`, `date_added`) VALUES
-(2, 'Alberto', 'Lumbreras', 'admin', '$2y$10$CQuSyOX9WPvQO8De8qaeAORHF2AsWLYcjsPms/GwhS/0yluIcd6Gq', '1730049@upv.edu.mx', '2020-06-01');
+(2, 'claudia', 'carrizales', 'admin', '$2y$10$CQuSyOX9WPvQO8De8qaeAORHF2AsWLYcjsPms/GwhS/0yluIcd6Gq', '1730048@upv.edu.mx', '2020-07-07');
 
 --
 -- Índices para tablas volcadas
