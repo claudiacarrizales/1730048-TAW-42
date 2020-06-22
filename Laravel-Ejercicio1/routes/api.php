106cf35c64c03225540658f5c1fb79d0ae16256d
claudiacarrizales <36921443+claudiacarrizales@users.noreply.github.com>
2020-06-22 08:48:33 -0500

    ejercicio 1

--- Laravel-Ejercicio1/routes/api.php
+++ Laravel-Ejercicio1/routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Empleado;
 
 //Listar empleados
+
 Route::get('empleados',function(){
     $empleados = Empleado::get();
     return $empleados;
 });
 
+//ruta para guardar nuevos empleados y recibir datos
 
 Route::post('empleados',function(Request $request){
     //Verificamos que los datos enviados se reciban bien para guardar en la bd,utilizamos Request
@@ -38,16 +40,48 @@ Route::post('empleados',function(Request $request){
     $empleado->email = $request->input('email');
     $empleado->lugar_nacimiento = $request->input('lugar_nacimiento');
     $empleado->sexo = $request->input('sexo');
-    $empleado->sexo = $request->input('estado_civil');
-    $empleado->sexo = $request->input('telefono');
+    $empleado->estado_civil = $request->input('estado_civil');
+    $empleado->telefono = $request->input('telefono');
 
     $empleado->save();
     return "Usuario creado";
-    
-
-
-
 });
+
+//para actualizar empleado
+Route::put('empleados/{id}', function(Request $request, $id){
+    $request->validate([
+        'nombres'=>'required|max:50',
+        'apellidos'=>'required|max:50',
+        'cedula'=>'required|max:20',
+        'email'=>'required|max:50|email|unique:empleados,email,' .$id,
+        'lugar_nacimiento'=>'required|max:50',
+        'sexo'=>'required|max:50',
+        'estado_civil'=>'required|max:50',
+        'telefono'=>'required|numeric'
+    ]);
+
+    $empleado = Empleado::findOrFail($id);
+    $empleado->nombres=$request->input('nombres');
+    $empleado->apellidos = $request->input('apellidos');
+    $empleado->cedula = $request->input('cedula');
+    $empleado->email = $request->input('email');
+    $empleado->lugar_nacimiento = $request->input('lugar_nacimiento');
+    $empleado->sexo = $request->input('sexo');
+    $empleado->estado_civil = $request->input('estado_civil');
+    $empleado->telefono = $request->input('telefono');
+
+    $empleados->save();
+    return "Empleado actualizado";
+});
+
+//ruta para eliminar empleados
+Route::delete('empleados/{id}', function($id){
+    $empleado=Empleado::findOrFail($id);
+    $empleado->delete();
+    return "Empleado eliminado";
+});
+
+
 //Comandos
 /*
 1.-Crear migracion: php artisan make:migration Nombre
